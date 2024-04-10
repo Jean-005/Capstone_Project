3aa8fc76ed57f2524f51b7335609b2060a357a9c
Tommybui28 <156359475+Tommybui28@users.noreply.github.com>
2024-04-10 15:55:48 +0100

    Driver controller initialise and add GetMapping method

--- capstone_backend/src/main/java/com/example/Capstone_Backend/controllers/DriverController.java
+++ capstone_backend/src/main/java/com/example/Capstone_Backend/controllers/DriverController.java
@@ -1,4 +1,28 @@
 package com.example.Capstone_Backend.controllers;
 
+
+import com.example.Capstone_Backend.models.Driver;
+import com.example.Capstone_Backend.services.DriverService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("drivers")
 public class DriverController {
+    @Autowired
+    DriverService driverService;
+
+    @GetMapping
+    public ResponseEntity<List<Driver>> getAllDrivers(){
+        List<Driver> allDrivers = driverService.getAllDrivers();
+        return new ResponseEntity<>(allDrivers, HttpStatus.OK);
+    }
+
+
 }
