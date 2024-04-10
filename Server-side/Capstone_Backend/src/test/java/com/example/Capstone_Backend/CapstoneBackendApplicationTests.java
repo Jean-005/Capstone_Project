f1ebf6cd0319625a1b9eff486844c759af0b2fd6
Jean <jean-ata@hotmail.co.uk>
2024-04-10 14:45:12 +0100

    added project files

---
+++ Server-side/Capstone_Backend/src/test/java/com/example/Capstone_Backend/CapstoneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Capstone_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
