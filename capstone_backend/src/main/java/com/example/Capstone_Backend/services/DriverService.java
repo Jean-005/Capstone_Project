bdeaa41a1039d9223a09ff2f7d259ebc63a5681e
Yesica Nithiyanantha <y.nithi123@gmail.com>
2024-04-10 16:34:46 +0100

    Add autowired annotation to driver service

--- capstone_backend/src/main/java/com/example/Capstone_Backend/services/DriverService.java
+++ capstone_backend/src/main/java/com/example/Capstone_Backend/services/DriverService.java
@@ -1,8 +1,13 @@
 package com.example.Capstone_Backend.services;
 
+import com.example.Capstone_Backend.repositories.DriverRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DriverService {
+
+    @Autowired
+    DriverRepository driverRepository;
     
 }
