a1ea3e91d2fb1bec8797bdd512365b1254895e7f
Jean <jean-ata@hotmail.co.uk>
2024-04-10 16:24:39 +0100

    Add getters and setters to Order

--- capstone_backend/src/main/java/com/example/Capstone_Backend/models/Order.java
+++ capstone_backend/src/main/java/com/example/Capstone_Backend/models/Order.java
@@ -30,12 +30,60 @@ public class Order {
     public Order() {
     }
 
-    public Order(Long id, int orderNumber, String pickupLocation, String deliveryLocation, boolean isDelivered, Route route) {
+    public Order(Long id, int orderNumber, String pickupLocation, String deliveryLocation, boolean isDelivered, Route route){
         this.id = id;
         this.orderNumber = orderNumber;
         this.pickupLocation = pickupLocation;
         this.deliveryLocation = deliveryLocation;
         this.isDelivered = isDelivered;
+        this.route = route;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(int orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public String getPickupLocation() {
+        return pickupLocation;
+    }
+
+    public void setPickupLocation(String pickupLocation) {
+        this.pickupLocation = pickupLocation;
+    }
+
+    public String getDeliveryLocation() {
+        return deliveryLocation;
+    }
+
+    public void setDeliveryLocation(String deliveryLocation) {
+        this.deliveryLocation = deliveryLocation;
+    }
+
+    public boolean isDelivered() {
+        return isDelivered;
+    }
+
+    public void setDelivered(boolean delivered) {
+        isDelivered = delivered;
+    }
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public void setRoute(Route route) {
         this.route = route;
     }
 }
