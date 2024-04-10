f107d5e1f53f628ac7cc30041371985bb33063fe
Tommybui28 <156359475+Tommybui28@users.noreply.github.com>
2024-04-10 16:08:55 +0100

    Add DriverRepository

--- capstone_backend/src/main/java/com/example/Capstone_Backend/repositories/DriverRepository.java
+++ capstone_backend/src/main/java/com/example/Capstone_Backend/repositories/DriverRepository.java
@@ -1,4 +1,9 @@
 package com.example.Capstone_Backend.repositories;
 
-public interface DriverRepository {
+import com.example.Capstone_Backend.models.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DriverRepository extends JpaRepository<Driver, Long> {
 }
