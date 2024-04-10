c3544e7471395ee24faac1606f85a65ad6ab711d
Aebel Shajan <aebelshajan@gmail.com>
2024-04-10 16:26:23 +0100

    Fix missing repository annotation

--- capstone_backend/src/main/java/com/example/Capstone_Backend/repositories/RouteRepository.java
+++ capstone_backend/src/main/java/com/example/Capstone_Backend/repositories/RouteRepository.java
@@ -4,5 +4,6 @@ import com.example.Capstone_Backend.models.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RouteRepository extends JpaRepository<Route, Long> {
 }
