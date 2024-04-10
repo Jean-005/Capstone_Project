96a3962ae91c745042ce21619419ffc2a84c0484
Jean <jean-ata@hotmail.co.uk>
2024-04-10 16:22:18 +0100

    Create a table for orders

--- capstone_backend/src/main/java/com/example/Capstone_Backend/models/Order.java
+++ capstone_backend/src/main/java/com/example/Capstone_Backend/models/Order.java
@@ -1,4 +1,28 @@
 package com.example.Capstone_Backend.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "orders")
 public class Order {
-}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private int orderNumber;
+
+    @Column
+    private String pickupLocation;
+
+    @Column
+    private String deliveryLocation;
+
+    @Column
+    private boolean isDelivered;
+
+    @JsonIgnoreProperties({"route"})
+    @ManyToOne
+    private Route route;
