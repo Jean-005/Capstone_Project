484cd5a49d199d00668ddfc41ad0a71c3a5d768a
Tommybui28 <156359475+Tommybui28@users.noreply.github.com>
2024-04-10 16:52:19 +0100

    Add service and Repository RouteService

--- capstone_backend/src/main/java/com/example/Capstone_Backend/services/RouteService.java
+++ capstone_backend/src/main/java/com/example/Capstone_Backend/services/RouteService.java
@@ -1,4 +1,14 @@
 package com.example.Capstone_Backend.services;
 
+import com.example.Capstone_Backend.repositories.DriverRepository;
+import com.example.Capstone_Backend.repositories.OrderRepository;
+import com.example.Capstone_Backend.repositories.RouteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RouteService {
+
+    @Autowired
+    RouteRepository routeRepository;
 }
