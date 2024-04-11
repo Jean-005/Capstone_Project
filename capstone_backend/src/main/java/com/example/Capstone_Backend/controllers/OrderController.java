2b0f722d0f559af520759470cbfadd8e5e753a1a
Yesica Nithiyanantha <y.nithi123@gmail.com>
2024-04-11 12:06:42 +0100

    Complete get all orders route

--- capstone_backend/src/main/java/com/example/Capstone_Backend/controllers/OrderController.java
+++ capstone_backend/src/main/java/com/example/Capstone_Backend/controllers/OrderController.java
@@ -1,9 +1,16 @@
 package com.example.Capstone_Backend.controllers;
 
+import com.example.Capstone_Backend.models.Order;
 import com.example.Capstone_Backend.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("orders")
@@ -12,5 +19,9 @@ public class OrderController {
     @Autowired
     OrderService orderService;
 
-
+    @GetMapping
+    public ResponseEntity<List<Order>> getAllOrders(){
+        List<Order> allOrders = orderService.getAllOrders();
+        return new ResponseEntity<>(allOrders, HttpStatus.OK);
+    }
 }
--- capstone_backend/src/main/java/com/example/Capstone_Backend/services/OrderService.java
+++ capstone_backend/src/main/java/com/example/Capstone_Backend/services/OrderService.java
@@ -1,12 +1,19 @@
 package com.example.Capstone_Backend.services;
 
+import com.example.Capstone_Backend.models.Order;
 import com.example.Capstone_Backend.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class OrderService {
+
     @Autowired
     OrderRepository orderRepository;
 
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
 }
