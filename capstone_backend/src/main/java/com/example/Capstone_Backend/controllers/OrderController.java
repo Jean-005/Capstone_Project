8ec96a0810cdf070690e4b88cce61c3497601e5c
Yesica Nithiyanantha <y.nithi123@gmail.com>
2024-04-11 12:28:55 +0100

    Start update order status by id

--- capstone_backend/src/main/java/com/example/Capstone_Backend/controllers/OrderController.java
+++ capstone_backend/src/main/java/com/example/Capstone_Backend/controllers/OrderController.java
@@ -1,9 +1,12 @@
 package com.example.Capstone_Backend.controllers;
 
+import com.example.Capstone_Backend.models.Order;
 import com.example.Capstone_Backend.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("orders")
@@ -12,5 +15,10 @@ public class OrderController {
     @Autowired
     OrderService orderService;
 
+    @PatchMapping (value = "/{id")
+    public ResponseEntity<Order> updateOrderStatusById(@PathVariable long id, @RequestBody boolean isDelivered){
+        Optional<Order> orderOptional = orderService.findOrderStatusById(id);
+        if (orderOptional)
+    }
 
 }
--- capstone_backend/src/main/java/com/example/Capstone_Backend/services/OrderService.java
+++ capstone_backend/src/main/java/com/example/Capstone_Backend/services/OrderService.java
@@ -3,10 +3,12 @@ package com.example.Capstone_Backend.services;
 import com.example.Capstone_Backend.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PatchMapping;
 
 @Service
 public class OrderService {
     @Autowired
     OrderRepository orderRepository;
+
 
 }
