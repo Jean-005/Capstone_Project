aa9c1c2185abf383c5227f48aebe899caa5d2536
Yesica Nithiyanantha <y.nithi123@gmail.com>
2024-04-11 11:29:12 +0100

    Complete get all drivers route

--- capstone_backend/src/main/java/com/example/Capstone_Backend/services/DriverService.java
+++ capstone_backend/src/main/java/com/example/Capstone_Backend/services/DriverService.java
@@ -1,8 +1,11 @@
 package com.example.Capstone_Backend.services;
 
+import com.example.Capstone_Backend.models.Driver;
 import com.example.Capstone_Backend.repositories.DriverRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DriverService {
@@ -10,5 +13,8 @@ public class DriverService {
     @Autowired
     DriverRepository driverRepository;
 
-    
+
+    public List<Driver> getAllDrivers() {
+        return driverRepository.findAll();
+    }
 }
