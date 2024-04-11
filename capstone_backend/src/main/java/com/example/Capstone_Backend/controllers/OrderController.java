03d6a74c86b805c7bc4a87a6b0513b154b2e2e97
Yesica Nithiyanantha <y.nithi123@gmail.com>
2024-04-11 12:11:17 +0100

    Complete get order by id route

--- capstone_backend/src/main/java/com/example/Capstone_Backend/controllers/OrderController.java
+++ capstone_backend/src/main/java/com/example/Capstone_Backend/controllers/OrderController.java
@@ -1,9 +1,16 @@
 package com.example.Capstone_Backend.controllers;
 
+import com.example.Capstone_Backend.models.Order;
 import com.example.Capstone_Backend.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("orders")
@@ -12,5 +19,12 @@ public class OrderController {
     @Autowired
     OrderService orderService;
 
-
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Order> getOrderById(@PathVariable long id){
+        Optional<Order> foundOrder = orderService.findOrderById(id);
+        if(foundOrder.isPresent()){
+            return new ResponseEntity<>(foundOrder.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
 }
--- capstone_backend/src/main/java/com/example/Capstone_Backend/services/OrderService.java
+++ capstone_backend/src/main/java/com/example/Capstone_Backend/services/OrderService.java
@@ -1,12 +1,18 @@
 package com.example.Capstone_Backend.services;
 
+import com.example.Capstone_Backend.models.Order;
 import com.example.Capstone_Backend.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class OrderService {
     @Autowired
     OrderRepository orderRepository;
 
+    public Optional<Order> findOrderById(long id) {
+        return orderRepository.findById(id);
+    }
 }
