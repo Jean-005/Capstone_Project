5e58acfb03ced2f5626353f50544ce015018998b
Aebel Shajan <aebelshajan@gmail.com>
2024-04-11 12:56:41 +0100

    Add deleteDriverById mapping to DriverController

--- capstone_backend/src/main/java/com/example/Capstone_Backend/services/DriverService.java
+++ capstone_backend/src/main/java/com/example/Capstone_Backend/services/DriverService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DriverService {
@@ -15,5 +16,13 @@ public class DriverService {
 
     public List<Driver> getAllDrivers(){
         return driverRepository.findAll();
+    }
+
+    public Optional<Driver> getDriverById(Long id) {
+        return driverRepository.findById(id);
+    }
+
+    public void deleteDriver(Long id) {
+        driverRepository.deleteById(id);
     }
 }
