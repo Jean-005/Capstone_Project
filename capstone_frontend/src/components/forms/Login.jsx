a1fbc8a4e54b7a1856870794fb6e7be3555db04f
Sahil Patel <sahilpatel1906@gmail.com>
2024-04-12 11:06:54 -0500

    Modified Login Page and Registration Page layout

--- capstone_frontend/src/components/forms/Login.jsx
+++ capstone_frontend/src/components/forms/Login.jsx
@@ -1,137 +1,59 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
+import { useState } from "react";
+import Registration from "./Registration";
 
 const Login = () => {
-    const [registerOpen, setRegisterOpen] = React.useState(false);
-    const [formData, setFormData] = React.useState({
+
+
+    const [formData, setFormData] = useState({
         username: '',
-        password: '',
-        firstName: '',
-        lastName: '',
-        role: '',
-        phone: '',
-        email: '',
+        password: ''
     });
 
-    const handleRegisterOpen = () => {
-        setRegisterOpen(true);
+    const handleInputChange = (event) => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        });
     };
 
-    const handleRegisterClose = () => {
-        setRegisterOpen(false);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const { username, password } = formData;
+        console.log(username)
+        console.log(password)
+
     };
 
     return (
         <div>
-            <Button variant="outlined" onClick={handleRegisterOpen}>
-                Register
-            </Button>
-            <Dialog
-                open={registerOpen}
-                onClose={handleRegisterClose}
-                PaperProps={{
-                    component: 'form',
-                    onSubmit: (event) => {
-                        event.preventDefault();
-                        const formData = new FormData(event.currentTarget);
-                        const formJson = Object.fromEntries(formData.entries());
-                        const username = formJson.firstName;
-                        const password = formJson.lastName;
-                        const role = formJson.role;
-                        const phone = formJson.phone;
-                        const email = formJson.email;
-                        handleRegisterClose();
-                    },
-                }}
-            >
-                <DialogTitle>Register</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        Please enter your details to register.
-                    </DialogContentText>
-                    <TextField
-                        autoFocus
-                        required
-                        margin="dense"
-                        id="firstName"
-                        name="firstName"
-                        label="First Name"
-                        type="text"
-                        fullWidth
-                        variant="standard"
-                    />
-                    <TextField
-                        required
-                        margin="dense"
-                        id="lastName"
-                        name="lastName"
-                        label="Last Name"
-                        type="text"
-                        fullWidth
-                        variant="standard"
-                    />
-
-                    <FormControl fullWidth required variant="standard">
-                        <InputLabel id="role-label">Company Role</InputLabel>
-                        <Select
-                            labelId="role-label"
-                            id="role"
-                            name="role"
-                            value={formData.role}
-                            onChange={(event) => setFormData({ ...formData, role: event.target.value })}
-                        >
-                            <MenuItem value="Admin">Admin</MenuItem>
-                            <MenuItem value="Driver">Driver</MenuItem>
-                        </Select>
-                    </FormControl>
-                    <TextField
-                        required
-                        margin="dense"
-                        id="phone"
-                        name="phone"
-                        label="Phone Number"
-                        type="tel"
-                        fullWidth
-                        variant="standard"
-                    />
-                    <TextField
-                        required
-                        margin="dense"
-                        id="email"
-                        name="email"
-                        label="Email"
-                        type="email"
-                        fullWidth
-                        variant="standard"
-                    />
-                    <TextField
-                        required
-                        margin="dense"
-                        id="password"
-                        name="password"
-                        label="Password"
-                        type="password"
-                        fullWidth
-                        variant="standard"
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleRegisterClose}>Cancel</Button>
-                    <Button type="submit">Register</Button>
-                </DialogActions>
-            </Dialog>
+            <div>
+                <h2>Login Form:</h2>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Username:
+                        <input
+                            type="text"
+                            name="username"
+                            value={formData.username}
+                            onChange={handleInputChange}
+                        />
+                    </label>
+                    <label>
+                        Password:
+                        <input
+                            type="password"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleInputChange}
+                        />
+                    </label>
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+            <Registration />
         </div>
     );
+
 }
 
 export default Login;
