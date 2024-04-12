eb62515a88345b0c92c7c411b9912e2d0d003ed9
Jean <jean-ata@hotmail.co.uk>
2024-04-12 16:14:07 +0100

    Add login modal using Material UI

--- capstone_frontend/src/components/forms/Login.jsx
+++ capstone_frontend/src/components/forms/Login.jsx
@@ -1,7 +1,80 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
 const Login = () => {
-    return (
-        <p>Login Form:</p>
-    );
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <h2>Login Form:</h2>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Open form dialog
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          component: 'form',
+          onSubmit: (event) => {
+            event.preventDefault();
+            const formData = new FormData(event.currentTarget);
+            const formJson = Object.fromEntries(formData.entries());
+            const username = formJson.username;
+            const password = formJson.password;
+            console.log('Username:', username);
+            console.log('Password:', password);
+            handleClose();
+          },
+        }}
+      >
+        <DialogTitle>Login</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please enter your username and password to login.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="username"
+            name="username"
+            label="Username"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            required
+            margin="dense"
+            id="password"
+            name="password"
+            label="Password"
+            type="password"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button type="submit">Login</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 }
 
 export default Login;
