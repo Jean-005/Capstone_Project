b40f0171493a1949f55296b91d774c3a528bbca1
Aebel Shajan <aebelshajan@gmail.com>
2024-04-15 19:43:58 +0100

    Fix incorrect entry of variables into constructor

--- capstone_backend/src/main/java/com/example/Capstone_Backend/components/DataLoader.java
+++ capstone_backend/src/main/java/com/example/Capstone_Backend/components/DataLoader.java
@@ -42,15 +42,15 @@ public class DataLoader implements ApplicationRunner {
         List<Double> pickupGeocode = Arrays.asList(51.501476, -0.140634);
         String pickupLocation = "Buckingham Palace, London, SW1A 1AA";
 
-        Order order1 = new Order(349823,pickupLocation ,"Great Russell St, London WC1B 3DG", Arrays.asList(51.51920852847477, -0.12724418034731264), pickupGeocode);
+        Order order1 = new Order(349823,pickupLocation ,"Great Russell St, London WC1B 3DG", Arrays.asList(-0.12724418034731264,51.51920852847477), pickupGeocode);
         orderRepository.save(order1);
-        Order order2 = new Order(543553,pickupLocation ,"London Zoo, Outer Cir, London NW1 4RY", Arrays.asList(51.53480565, -0.15480659230689), pickupGeocode);
+        Order order2 = new Order(543553,pickupLocation ,"London Zoo, Outer Cir, London NW1 4RY", Arrays.asList(-0.15480659230689, 51.53480565), pickupGeocode);
         orderRepository.save(order2);
         Order order3 = new Order(234523,pickupLocation ,"Cross Bones Garden, Union St, London,  SE1 1TA", Arrays.asList(-0.0919393, 51.503344), pickupGeocode);
         orderRepository.save(order3);
         Order order4 = new Order(342323,pickupLocation , "32 London Bridge St, London SE1 9SG, United Kingdom", Arrays.asList(-0.0864458928542395, 51.5043421), pickupGeocode);
         orderRepository.save(order4);
-        Order order5 = new Order(776543,pickupLocation ,"Royal Albert Hall, Kensington Gore, South Kensington, London SW7 2AP", Arrays.asList(51.500942, -0.177498), pickupGeocode);
+        Order order5 = new Order(776543,pickupLocation ,"Royal Albert Hall, Kensington Gore, South Kensington, London SW7 2AP", Arrays.asList(-0.177498, 51.500942), pickupGeocode);
         orderRepository.save(order5);
 
         Route route1 = new Route(Arrays.asList(order1, order2, order3), aebel, 100, 180);
