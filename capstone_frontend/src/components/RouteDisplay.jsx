a7c67eb1abab609eeb856e2a4142e8fd8a0464d4
Yesica Nithiyanantha <y.nithi123@gmail.com>
2024-04-16 14:31:22 +0100

    Add Leaflet MapContainer to RouteDisplay

--- capstone_frontend/src/components/RouteDisplay.jsx
+++ capstone_frontend/src/components/RouteDisplay.jsx
@@ -1,9 +1,22 @@
-const RouteDisplay = () => {
+import {
+    TileLayer,
+    MapContainer,
+    LayersControl
+  } from "react-leaflet";
+const RouteDisplay = ({ routes }) => {
+    const [map, setMap] = useState(null);
+  
     return (
-        <p>
-            Route Display
-        </p>
+      <>
+      <MapContainer
+        center={[37.0902, -95.7129]}
+        zoom={3}
+        zoomControl={false}
+        style={{ height: "100vh", width: "100%", padding: 0 }}
+        whenCreated={map => setMap(map)}
+      ></MapContainer>
+      </>
     );
-}
+  };
 
 export default RouteDisplay;
