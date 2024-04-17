ee00a3224b9536a172ec66cee7d2336fadf0e065
Tommybui28 <156359475+Tommybui28@users.noreply.github.com>
2024-04-17 11:34:48 +0100

    Add @PostMapping to RouteController

--- capstone_backend/src/main/java/com/example/Capstone_Backend/controllers/RouteController.java
+++ capstone_backend/src/main/java/com/example/Capstone_Backend/controllers/RouteController.java
@@ -5,10 +5,7 @@ import com.example.Capstone_Backend.services.RouteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -36,5 +33,10 @@ public class RouteController {
         else {
             return new ResponseEntity<>(routes.get(0), HttpStatus.OK);
         }
+    }
+    @PostMapping
+    public ResponseEntity<Route> saveRoute(@RequestBody Route route){
+        Route newRoute = routeService.saveRoute(route);
+        return new ResponseEntity<>(newRoute, HttpStatus.CREATED);
     }
 }
