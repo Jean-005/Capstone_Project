2f7ec898cec07029c1c4a066761fea6c733600e1
onlysilvercrow <sahilpatel1906@gmail.com>
2024-04-17 09:15:05 -0500

    created initial template for help page

--- capstone_frontend/src/components/Help.jsx
+++ capstone_frontend/src/components/Help.jsx
@@ -1,10 +1,12 @@
 const Help = ({}) => {
 
     return (
-        <p>
-           Help Page
-        </p>
-
+        <>
+            <h2>Contact Us:</h2>
+            <p>Email: JeaniusReactAebels@fakeEmail.com</p>
+            <p>Phone: 032484092</p>
+            <p>Address: 80 Middlesex Street, London, E1 7EZ</p>
+        </>
     );
 }
 
