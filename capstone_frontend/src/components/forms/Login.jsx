abc7ffac7b306a78d092f4a68ed4aa0abec49804
Aebel Shajan <aebelshajan@gmail.com>
2024-04-17 10:20:37 +0100

    Update Login.jsx from main

--- capstone_frontend/src/components/forms/Login.jsx
+++ capstone_frontend/src/components/forms/Login.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Registration from "./Registration";
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { useNavigate } from "react-router-dom";
+
 
 
 const Login = ({ drivers, handleUserLogin, addNewDriver }) => {
@@ -23,57 +25,64 @@ const Login = ({ drivers, handleUserLogin, addNewDriver }) => {
         event.preventDefault();
         const { username, password } = formData;
 
-    if (!username.trim()) {
-        alert("Username is required");
-        return;
-    }
+        if (!username.trim()) {
+            alert("Username is required");
+            return;
+        }
 
-    if (!password.trim()) {
-        alert("Password is required");
-        return;
-    }
+        if (!password.trim()) {
+            alert("Password is required");
+            return;
+        }
 
-    if (driverExistence !== -1 && passwordExistence !== -1) {
-        handleUserLogin(currentDriver);
-        navigate("/driver")
-    }
-      
-    if (password.length < 6) {
-        alert("Password must be at least 6 characters long");
-        return;
-    }
-      
-};
+        if (password.length < 6) {
+            alert("Password must be at least 6 characters long");
+            return;
+        }
+
+        const driverExistence = drivers.findIndex(driver => driver.name === username);
+        const currentDriver = drivers[driverExistence]
+        const passwordExistence = drivers.findIndex(driver => driver.password === password);
+
+        if (driverExistence !== -1 && passwordExistence !== -1) {
+            handleUserLogin(currentDriver);
+            navigate("/driver")
+
+        }
+        else { alert("Wrong Username or Password"); }
+
+
+    };
 
     return (
         <div>
             <div>
                 <h2>Login Form:</h2>
                 <form onSubmit={handleSubmit}>
-                <TextField
-                    autoFocus
-                    required
-                    margin="dense"
-                    id="username"
-                    name="username"
-                    label="Username"
-                    type="text"
-                    fullWidth
-                    variant="standard"
-                    onChange={handleInputChange}
-                />
-                <TextField
-                    required
-                    margin="dense"
-                    id="password"
-                    name="password"
-                    label="Password"
-                    type="password"
-                    fullWidth
-                    variant="standard"
-                    onChange={handleInputChange}
+                    <TextField
+                        autoFocus
+                        required
+                        margin="dense"
+                        id="username"
+                        name="username"
+                        label="Username"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        onChange={handleInputChange}
+                    />
+                    <TextField
+                        required
+                        margin="dense"
+                        id="password"
+                        name="password"
+                        label="Password"
+                        type="password"
+                        fullWidth
+                        variant="standard"
+                        onChange={handleInputChange}
 
-                />
+                    />
                     <Button variant="outlined" type="submit">Login</Button>
                 </form>
             </div>
